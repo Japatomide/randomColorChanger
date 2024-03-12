a1685a6219ddd0163e771e65f7c3d18b501faa63
Japatomide <jaiyeolapaul68@gmail.com>
2024-03-12 12:29:04 +0100

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+// Targeted HTMl elements 
 let container = document.querySelector('#container')
 let button = document.querySelector('a')
 let displayHex = document.querySelector('h1')
 
+// a string of valid characters in regards to hex numbering systems
 const validCharacters = '0123456789ABCDEF';
 const hexDecimalLength = 6;
 
+// function to get a random integer
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max-min+1) + min)
 }
 
+// function to get the next random color
 function getNextRandomColor() {
     let color = '#'
     for (let i = 0; i < 6; i++) {
